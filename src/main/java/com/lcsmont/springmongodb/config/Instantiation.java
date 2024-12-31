189592eade8b98b559d5f9705032d59305d47c4c
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-31 16:49:45 -0300

    Referencing posts from User

--- src/main/java/com/lcsmont/springmongodb/config/Instantiation.java
+++ src/main/java/com/lcsmont/springmongodb/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning!", "I woke up happy today", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(p1, p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1, p2));
+        userRepository.save(maria);
     }
 }
