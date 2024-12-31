9c7f72d815539ff42bd336750eea24dd192ce90e
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-31 15:33:27 -0300

    Find user by Id

--- src/main/java/com/lcsmont/springmongodb/resource/UserResource.java
+++ src/main/java/com/lcsmont/springmongodb/resource/UserResource.java
@@ -6,6 +6,7 @@ import com.lcsmont.springmongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,11 @@ public class UserResource {
         List<User> users = userService.findAll();
         List<UserDTO> userDTOs = users.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOs);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+            User user = userService.findById(id);
+            return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
