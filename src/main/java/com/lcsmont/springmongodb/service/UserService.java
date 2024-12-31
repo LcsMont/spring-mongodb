630d544ed9e22b3104b7aa015cc77ebb12d4666b
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-31 15:48:51 -0300

    Inserting user w/ POST

--- src/main/java/com/lcsmont/springmongodb/service/UserService.java
+++ src/main/java/com/lcsmont/springmongodb/service/UserService.java
@@ -1,6 +1,7 @@
 package com.lcsmont.springmongodb.service;
 
 import com.lcsmont.springmongodb.domain.User;
+import com.lcsmont.springmongodb.dto.UserDTO;
 import com.lcsmont.springmongodb.repository.UserRepository;
 import com.lcsmont.springmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
