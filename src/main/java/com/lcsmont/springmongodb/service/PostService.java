923df69041942689e7e28c34f0f57e63e67ff745
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-31 16:59:23 -0300

    Get Post by id

---
+++ src/main/java/com/lcsmont/springmongodb/service/PostService.java
@@ -0,0 +1,26 @@
+package com.lcsmont.springmongodb.service;
+
+import com.lcsmont.springmongodb.domain.Post;
+import com.lcsmont.springmongodb.repository.PostRepository;
+import com.lcsmont.springmongodb.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+    }
+}
